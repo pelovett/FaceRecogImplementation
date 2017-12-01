92fdb702bb931dee82a0c128ee80329203ae386e
pelovett <pelovett@gmail.com>
2017-12-01 14:25:16 -0800

    Added method for retrieval of data

---
+++ GetData.py
@@ -0,0 +1,25 @@
+import os
+import urllib.request
+import zipfile
+
+def main():
+  dl_link = "http://vision.ucsd.edu/extyaleb/CroppedYaleBZip/CroppedYale.zip"
+  dir_path = os.path.dirname(os.path.realpath(__file__))
+  getunzipped(dl_link, dir_path+"/data/")
+
+#https://stackoverflow.com/questions/1774434/download-a-zip-file-to-a-local-drive-and-extract-all-files-to-a-destination-fold
+
+def getunzipped(theurl, thedir):
+  name = os.path.join(thedir, 'temp.zip')
+  name, hdrs = urllib.request.urlretrieve(theurl, name)
+  try:
+    z = zipfile.ZipFile(name, 'r')
+  except:
+    print("Bad zipfile (from %r):" % (theurl))
+    return
+  z.extractall(thedir)
+  z.close()
+  os.unlink(name)
+
+if __name__ == "__main__":
+  main()
